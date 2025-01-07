62692c8daa40f77ce62a384e685ed4d877d42055
Your Name <ekka.beaula@gmail.com>
2025-01-07 22:13:13 +0100

    making api calls

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World and universe</h1>");
+//set up static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "about.html"));
+// });
+
+let posts = [
+  { id: 1, title: "Post one" },
+  { id: 2, title: "Post two" },
+  { id: 3, title: "Post three" },
+  { id: 4, title: "Post four" },
+  { id: 5, title: "Post five" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
 });
 
 app.listen(8000, () => console.log("Server is running on port 8000"));
