b118915a22189aacda3786e506e27b054f10db74
Your Name <ekka.beaula@gmail.com>
2025-01-10 14:43:48 +0100

    color code -complete

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,19 @@
+import colors from "colors";
 //logger
 //next runs at the end and adds next piece of function
 const logger = (req, res, next) => {
+  const methodColors = {
+    GET: "green",
+    POST: "blue",
+    PUT: "yellow",
+    DELETE: "red",
+  };
+
+  const color = methodColors[req.method] || white;
   console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`[
+      color
+    ]
   );
   next();
 };
