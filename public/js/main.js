b2e365e1cbec3fb8810bb5de1079e540e1b3ab6d
Your Name <ekka.beaula@gmail.com>
2025-01-13 10:45:36 +0100

    main-update

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,23 @@ const button = document.querySelector("#get-post-btn");
 
 //Get and show posts
 async function showPosts() {
-  const res = await fetch("https//localhost:8000/api/posts");
-  if (!res) {
-    throw new Error("Failed to fetch posts");
-  }
-  const posts = await res.json();
-  output.innerHTML = "";
+  try {
+    const res = await fetch("https//localhost:8000/api/posts");
+    if (!res) {
+      throw new Error("Failed to fetch posts");
+    }
+    const posts = await res.json();
+    output.innerHTML = "";
 
-  posts.forEach((post)=>{
-    const postElement = document.createElement("div");
-  })
+    posts.forEach((post) => {
+      const postEl = document.createElement("div");
+      postEl.textContent = post.title;
+      output.appendChild(postEl);
+    });
+  } catch (error) {
+    console.log("Error fetching posts:", error);
+  }
 }
+
+//Event Listners
+button.addEventListners("click", showPosts);
